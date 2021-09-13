e13cf7acc07263bbcb116a90de54cbd58336dd6e
Sergey <piolersa85@mail.ru>
2021-09-13 20:08:18 +0200

    Initial Commit

---
+++ Step 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Step 3
+//
+//  Created by Sergey on 13.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
